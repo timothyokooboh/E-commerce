24bdc508634679ed48fc5f98f8060ff9f724ea67
timothyokooboh <okoobohtimothy@gmail.com>
2020-06-19 18:27:09 -0700

    created navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <v-app-bar app flat color="#241663" class="white--text">
+
+            <v-app-bar-nav-icon color="#fff" @click = "drawer =! drawer"> </v-app-bar-nav-icon>
+
+            <v-toolbar-title class = "font-weight-bold" color>
+                <v-btn text color="#fff">
+                    BUY-hub
+                </v-btn>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-tooltip bottom color="#e14594">
+                <template v-slot:activator="{on}">
+                    <v-btn text color="#fff" v-on="on">
+                        <span class="mx-3"> Cart </span>
+                    <v-icon>mdi-cart</v-icon>
+                    </v-btn>
+                </template>
+                <div> View Cart </div>
+            </v-tooltip>
+            
+        </v-app-bar>
+
+        <v-navigation-drawer app disable-resize-watcher v-model = "drawer" color="#241663"></v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+      return {
+          drawer: false
+      }
+  },
+  components: {
+    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <div class = "flex-container">
+        <div class="input-container mx-5">
+          <div class="input">
+            <input type="text">
+          </div>
+          <div>
+            <v-btn rounded small class="search-btn white--text" color="#241663">Search</v-btn>
+          </div>
+        </div>
+        <div>
+          <v-btn text color="#fff">
+            <span>FILTER</span>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style scoped>
+  .home {
+    width: 100%;
+    height: 100%;
+    background: var(--primary-color)
+  }
+  .flex-container {
+    width: 100%;
+    display: flex;
+    align-items: baseline;
+  }
+  .input-container {
+    display: flex;
+    width: 90%;
+  }
+  .input {
+    width: 100%;
+  }
+  .input-container input {
+    width: 100%;
+    background: #fff;
+    height: 35px;
+    border-radius: 20px;
+    outline: none;
+    padding: 10px;
+    color: var(--tertiary-color);
+    font-weight: bold;
+  }
+  .search-btn {
+    margin-left: -80px;
+    margin-top: 4px;
+  }
+</style>
